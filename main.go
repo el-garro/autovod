ae07957b18042035182129b0dfbe10316fabddee
Nyan <alexem9692@gmail.com>
2025-10-15 12:16:52 -0400

    misc

--- main.go
+++ main.go
@@ -12,7 +12,14 @@ func main() {
 	log.Info("Initializing...")
 
 	LoadConfig()
-	log.Info("Config", "TWITCH_CLIENT_ID", Config.TwitchClientID, "TWITCH_CHANNEL", Config.TwitchChannel, "TWITCH_VIDEO_HEIGHT", Config.TwitchVideoHeight)
+	log.Info(
+		"Config:",
+		"TWITCH_CLIENT_ID", Config.TwitchClientID,
+		"TWITCH_CHANNEL", Config.TwitchChannel,
+		"TWITCH_VIDEO_HEIGHT", Config.TwitchVideoHeight,
+		"DELETE_VOD_AFTER", Config.DeleteAfter,
+		"WEB_SERVER_PORT", Config.WebPort,
+	)
 
 	log.Info("Starting services...")
 
